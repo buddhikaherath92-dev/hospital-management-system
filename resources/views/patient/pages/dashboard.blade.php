458852e5d06d1f6a91670974f00c95e9133ad95b
KevinBoy98 <kavindapushpitha0@gmail.com>
2019-02-21 13:57:07 +0530

    Patient Dashboard frontend implemented

--- resources/views/patient/pages/dashboard.blade.php
+++ resources/views/patient/pages/dashboard.blade.php
@@ -32,6 +32,121 @@
                                         </span>
                                     @endif
                                 </div>
+                                <div class="form-group">
+                                    <label>Address</label>
+                                    <input type="text" class="form-control" name="address" value="{{ old('address') }}">
+                                    @if ($errors->has('address'))
+                                        <span class="invalid-feedback" style="display: block">
+                                            <small>{{ $errors->first('address') }}</small>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-4">
+                                        <label>Contact No</label>
+                                        <input type="tel" class="form-control" name="tel_no" value="{{ old('tel_no') }}">
+                                        @if ($errors->has('tel_no'))
+                                            <span class="invalid-feedback" style="display: block">
+                                                <small>{{ $errors->first('tel_no') }}</small>
+                                             </span>
+                                        @endif
+                                    </div>
+
+                                    <div class="form-group col-md-4">
+                                        <label>Gender</label>
+                                        <select name="gender" class="form-control">
+                                            <option selected>Male</option>
+                                            <option>Female</option>
+                                            <option>Other</option>
+                                        </select>
+                                        @if ($errors->has('gender'))
+                                            <span class="invalid-feedback" style="display: block">
+                                                <small>{{ $errors->first('gender') }}</small>
+                                            </span>
+                                        @endif
+                                    </div>
+
+                                    <div class="form-group col-md-4">
+                                        <label>Blood Group</label>
+                                        <select name="blood_group" class="form-control">
+                                            <option selected>O+</option>
+                                            <option>O-</option>
+                                            <option>A+</option>
+                                            <option>A-</option>
+                                            <option>B+</option>
+                                            <option>B-</option>
+                                            <option>AB+</option>
+                                            <option>AB-</option>
+                                        </select>
+                                        @if ($errors->has('blood_group'))
+                                            <span class="invalid-feedback" style="display: block">
+                                                <small>{{ $errors->first('blood_group') }}</small>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-3">
+                                        <label>Age</label>
+                                        <input type="text" class="form-control" name="age" value="{{ old('age') }}">
+                                        @if ($errors->has('age'))
+                                            <span class="invalid-feedback" style="display: block">
+                                                <small>{{ $errors->first('age') }}</small>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group col-md-3">
+                                        <label>Height</label>
+                                        <input type="text" class="form-control" name="height" value="{{ old('height') }}">
+                                        @if ($errors->has('height'))
+                                            <span class="invalid-feedback" style="display: block">
+                                                <small>{{ $errors->first('height') }}</small>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group col-md-3">
+                                        <label>Weight</label>
+                                        <input type="text" class="form-control" name="weight" value="{{ old('weight') }}">
+                                        @if ($errors->has('weight'))
+                                            <span class="invalid-feedback" style="display: block">
+                                                <small>{{ $errors->first('weight') }}</small>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group col-md-3">
+                                        <label>Patient Category</label>
+                                        <input type="text"name="patient_category" class="form-control">
+                                        @if ($errors->has('patient_category'))
+                                            <span class="invalid-feedback" style="display: block">
+                                                <small>{{ $errors->first('patient_category') }}</small>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label >Allergies</label>
+                                    <textarea type="text" class="form-control" name="allergies" ></textarea>
+                                    @if ($errors->has('allergies'))
+                                        <span class="invalid-feedback" style="display: block">
+                                            <small>{{ $errors->first('allergies') }}</small>
+                                        </span>
+                                    @endif
+                                </div>
+
+                                <div class="form-group">
+                                    <label >Medical Condition</label>
+                                    <textarea type="text" class="form-control" name="medical_condition"></textarea>
+                                    @if ($errors->has('medical_condition'))
+                                        <span class="invalid-feedback" style="display: block">
+                                            <small>{{ $errors->first('medical_condition') }}</small>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="form-group">
+                                    <div class="float-right">
+                                        <button type="submit" class="btn btn-success ">Save Changes</button>
+                                    </div>
+                                </div>
                             </form>
                         </div>
                     </div>
