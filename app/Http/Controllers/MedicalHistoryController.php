27347f881587e66c7de79542f40877b5934dfc30
Buddhika Herath <buddhikaherath.dev@gmail.com>
2019-08-12 19:56:43 +0530

    Patients dashboard, filter prescriptions by doctor name - back-end functionality

--- app/Http/Controllers/MedicalHistoryController.php
+++ app/Http/Controllers/MedicalHistoryController.php
@@ -27,15 +27,25 @@ class MedicalHistoryController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function show(){
+
+        $prescriptions = DB::table('prescriptions')
+            ->select('prescriptions.prescription','diagnoses.diagnose','prescriptions.diagnose_id')
+            ->join('diagnoses','prescriptions.diagnose_id','=','diagnoses.id')
+            ->where('patient_id',Patient::where('user_id',Auth::id())->value('id'));
+
+        // filter prescriptions by doctor name
+        if(request()->has('filter_by') && request()->has('filter_param') &&
+            request('filter_by') === 'prescription_by_doc'){
+            $prescriptions = $prescriptions
+                ->join('users', 'diagnoses.doctor_id', '=', 'users.id')
+                ->where('users.name', 'LIKE', '%'.request('filter_param').'%' );
+        }
+
         return view('patient.pages.medical_history',[
             'diagnoses'=>Diagnose::joinDiagnose()->where('patients.id',Patient::getPatient(Auth::id())['id'])->get(),
             'reports'=>PatientReport::where('patient_id',Auth::id())->get(),'events'=>Event::where('patient_id',Auth::id())->get(),
             'doctor_reports'=>Laboratory::where('patient_id',Patient::where('user_id',Auth::id())->value('id'))->get(),
-            'prescriptions'=> DB::table('prescriptions')
-                ->select('prescriptions.prescription','diagnoses.diagnose','prescriptions.diagnose_id')
-                ->join('diagnoses','prescriptions.diagnose_id','=','diagnoses.id')
-                ->where('patient_id',Patient::where('user_id',Auth::id())->value('id'))
-                ->get()
+            'prescriptions'=> $prescriptions->get()
         ]);
     }
 }
