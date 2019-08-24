9d5c90c9c6cadb5a63a95c198c3c2eab6d9cf91e
Buddhika Herath <buddhikaherath.dev@gmail.com>
2019-08-24 08:50:45 +0530

    Filter options implemented on Lab dashboard

--- app/Http/Controllers/Lab/RequestController.php
+++ app/Http/Controllers/Lab/RequestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Lab;
 
 use App\Diagnose;
 use App\Laboratory;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,8 +15,37 @@ class RequestController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function show(){
+        $doctors = User::where('user_type', (int)2)->get();
+        $requests = Diagnose::getAllRequests();
+        $filterParams = [];
+
+        if(request()->has('filter_enabled') && request('filter_enabled') === 'true'){
+            if(request()->has('report_title') && request('report_title') !== ''){
+                $requests = $requests->where('title', 'like', '%'.request('report_title').'%');
+                $filterParams['report_title'] = request('report_title');
+            }
+            if(request()->has('patient_name') && request('patient_name') !== null){
+                $requests = $requests->where('full_name', 'like', '%'.request('patient_name').'%');
+                $filterParams['patient_name'] = request('patient_name');
+            }
+            if(request()->has('doctor_name') && request('doctor_name') !== null){
+                $requests = $requests->where('users.id', request('doctor_name'));
+                $filterParams['doctor_name'] = request('doctor_name');
+            }
+            if(request()->has('report_status') && request('report_status') !== null){
+                $requests = $requests->where('laboratories.is_ready', request('report_status'));
+                $filterParams['report_status'] = request('report_status');
+            }
+            if(request()->has('posted_date') && request('posted_date') !== null){
+                $requests = $requests->whereDate('posted_date', request('posted_date'));
+                $filterParams['posted_date'] = request('posted_date');
+            }
+        }
+
         return view('lab.pages.all_requests',[
-            'requests'=>Diagnose::getAllRequests()->get()
+            'requests'=>$requests->get(),
+            'doctors'=>$doctors,
+            'filterParams' => $filterParams
         ]);
     }
 }
--- resources/views/lab/pages/all_requests.blade.php
+++ resources/views/lab/pages/all_requests.blade.php
@@ -1,6 +1,65 @@
 @extends('lab.layouts.dashboard')
 
 @section('child-content')
+    <div class="bg-dark p-3 mb-3">
+        <form class="form-inline" action="{{route('show_requests')}}" method="get">
+            <input type="text" class="form-control mb-2 mr-sm-2"
+                   id="inlineFormInputName2" name="report_title"
+                   value="{{count($filterParams) > 0 && isset($filterParams['report_title']) ?
+                   $filterParams['report_title'] : '' }}"
+                   placeholder="Search by Report Title:">
+            <input type="text" class="form-control mb-2 mr-sm-2"
+                   id="inlineFormInputName2" name="patient_name"
+                   value="{{count($filterParams) > 0 && isset($filterParams['patient_name']) ?
+                   $filterParams['patient_name'] : '' }}"
+                   placeholder="Search by Patient Name:">
+            <div class="input-group mb-2 mr-sm-2">
+                <select class="custom-select" name="doctor_name">
+                    <option value=""
+                        {{count($filterParams) > 0 && isset($filterParams['doctor_name'])
+                        && $filterParams['doctor_name'] === '' ? 'selected' : ''}}>
+                        All Doctors
+                    </option>
+                    @foreach($doctors as $docIndex => $doctor)
+                        <option value="{{$doctor->id}}"
+                        {{count($filterParams) > 0 && isset($filterParams['doctor_name'])
+               && $filterParams['doctor_name'] == $doctor->id ? 'selected' : ''}}>
+                            {{$doctor->name}}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="input-group mb-2 mr-sm-2">
+                <select class="custom-select" name="report_status">
+                    <option value=""
+                        {{count($filterParams) > 0 && isset($filterParams['report_status'])
+                           && $filterParams['report_status'] === '' ? 'selected' : ''}}>
+                        Any Status
+                    </option>
+                    <option value="1"
+                        {{count($filterParams) > 0 && isset($filterParams['report_status'])
+                               && $filterParams['report_status'] === '1' ? 'selected' : ''}}>
+                        Ready
+                    </option>
+                    <option value="2"
+                        {{count($filterParams) > 0 && isset($filterParams['report_status'])
+                               && $filterParams['report_status'] === '2' ? 'selected' : ''}}>
+                        Pending
+                    </option>
+                </select>
+            </div>
+            <input type="date" class="form-control mb-2 mr-sm-2"
+                   id="inlineFormInputName2" name="posted_date"
+                   value="{{count($filterParams) > 0 && isset($filterParams['posted_date'])?
+                   $filterParams['posted_date'] : ''}}">
+
+                <input type="hidden" name="filter_enabled" value="true">
+
+            <button type="submit" class="btn btn-primary mb-2">Search</button>  &nbsp; &nbsp;
+            <a href="{{route('show_requests')}}" class="btn btn-info mb-2">Reset Filters</a>
+        </form>
+    </div>
+
     <h4 class="">All Report Requests</h4>
     <hr><br>
     <table class="table table-hover">
@@ -17,6 +76,11 @@
         </tr>
         </thead>
         <tbody>
+        @if(count($requests) === 0)
+            <tr>
+                <td colspan="7"><h6 class="text-center">Sorry no relavant records found!</h6></td>
+            </tr>
+        @endif
         @foreach($requests as $request)
             <tr >
                 <th scope="row">{{ $request['id'] }}</th>
