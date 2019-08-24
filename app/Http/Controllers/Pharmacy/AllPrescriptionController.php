3c3ce8add18b5b576a2bf8880a76c4a839e36680
Buddhika Herath <buddhikaherath.dev@gmail.com>
2019-08-24 18:37:30 +0530

    Filter options implemented on Pharmacy dashboard

--- app/Http/Controllers/Pharmacy/AllPrescriptionController.php
+++ app/Http/Controllers/Pharmacy/AllPrescriptionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Pharmacy;
 
 use App\Diagnose;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -13,9 +14,33 @@ class AllPrescriptionController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function show(){
-//        dd(Diagnose::getNotReadyedPrescriptions()->get());
+        $diagnoses = Diagnose::getNotReadyedPrescriptions();
+        $doctors = User::where('user_type', (int)2)->get();
+        $filterParams = [];
+
+        if(request()->has('filter_enabled') && request('filter_enabled') === 'true'){
+            if(request()->has('patient_name') && request('patient_name') !== null){
+                $diagnoses = $diagnoses->where('full_name', 'like', '%'.request('patient_name').'%');
+                $filterParams['patient_name'] = request('patient_name');
+            }
+            if(request()->has('doctor_name') && request('doctor_name') !== null){
+                $diagnoses = $diagnoses->where('users.id', request('doctor_name'));
+                $filterParams['doctor_name'] = request('doctor_name');
+            }
+            if(request()->has('report_status') && request('report_status') !== null){
+                $diagnoses = $diagnoses->where('pharmacies.is_ready', request('report_status'));
+                $filterParams['report_status'] = request('report_status');
+            }
+            if(request()->has('posted_date') && request('posted_date') !== null){
+                $diagnoses = $diagnoses->whereDate('posted_date', request('posted_date'));
+                $filterParams['posted_date'] = request('posted_date');
+            }
+        }
+
         return view('pharmacy.pages.all_prescriptions',[
-            'diagnoses'=>Diagnose::getNotReadyedPrescriptions()->get()
+            'diagnoses'=>$diagnoses->get(),
+            'filterParams' => $filterParams,
+            'doctors' => $doctors
         ]);
     }
 }
--- resources/views/pharmacy/pages/all_prescriptions.blade.php
+++ resources/views/pharmacy/pages/all_prescriptions.blade.php
@@ -1,6 +1,60 @@
 @extends('pharmacy.layouts.dashboard')
 
 @section('child-content')
+    <div class="bg-dark p-3 mb-3">
+        <form class="form-inline" action="{{route('show_all_prescriptions')}}" method="get">
+            <input type="text" class="form-control mb-2 mr-sm-2"
+                   id="inlineFormInputName2" name="patient_name"
+                   value="{{count($filterParams) > 0 && isset($filterParams['patient_name']) ?
+                   $filterParams['patient_name'] : '' }}"
+                   placeholder="Search by Patient Name:">
+            <div class="input-group mb-2 mr-sm-2">
+                <select class="custom-select" name="doctor_name">
+                    <option value=""
+                        {{count($filterParams) > 0 && isset($filterParams['doctor_name'])
+                        && $filterParams['doctor_name'] === '' ? 'selected' : ''}}>
+                        All Doctors
+                    </option>
+                    @foreach($doctors as $docIndex => $doctor)
+                        <option value="{{$doctor->id}}"
+                            {{count($filterParams) > 0 && isset($filterParams['doctor_name'])
+                   && $filterParams['doctor_name'] == $doctor->id ? 'selected' : ''}}>
+                            {{$doctor->name}}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="input-group mb-2 mr-sm-2">
+                <select class="custom-select" name="report_status">
+                    <option value=""
+                        {{count($filterParams) > 0 && isset($filterParams['report_status'])
+                           && $filterParams['report_status'] === '' ? 'selected' : ''}}>
+                        Any Status
+                    </option>
+                    <option value="1"
+                        {{count($filterParams) > 0 && isset($filterParams['report_status'])
+                               && $filterParams['report_status'] === '1' ? 'selected' : ''}}>
+                        Ready
+                    </option>
+                    <option value="2"
+                        {{count($filterParams) > 0 && isset($filterParams['report_status'])
+                               && $filterParams['report_status'] === '2' ? 'selected' : ''}}>
+                        Pending
+                    </option>
+                </select>
+            </div>
+            <input type="date" class="form-control mb-2 mr-sm-2"
+                   id="inlineFormInputName2" name="posted_date"
+                   value="{{count($filterParams) > 0 && isset($filterParams['posted_date'])?
+                   $filterParams['posted_date'] : ''}}">
+
+            <input type="hidden" name="filter_enabled" value="true">
+
+            <button type="submit" class="btn btn-primary mb-2">Search</button>  &nbsp; &nbsp;
+            <a href="{{route('show_all_prescriptions')}}" class="btn btn-info mb-2">Reset Filters</a>
+        </form>
+    </div>
+
     <h4 class="">All Prescriptions</h4>
     <hr><br>
     <table class="table table-hover">
